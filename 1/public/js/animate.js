4dcd1ed445c43a34eff3b580c9ac921c99b6a8d9
fanfan <772377118@qq.com>
2020-04-23 22:19:54 +0800

    121212

---
+++ 1/public/js/animate.js
@@ -0,0 +1,39 @@
+
+$(function(){
+  // let animate = (dom, time) => {
+  //   dom.css('transform','translateY(0)')
+  // }
+  
+  // $('.textanimation').css('transform','translateY(0)'); // 文字效果
+  // $('.textanimation').animate({'transform':'translateY(0)'}) // 文字效果
+
+  $('.navbox').on('click',function(){ // 导航栏切换
+    let right = $('.navbar').css('right');
+    if(right == '0px') {
+      $('.meng').hide();
+      $('.navbar').css('right','-100%');
+      $('.navbox').show();
+      $('.navclose').hide();
+    } else {
+      $('.navbar').css('right','0');
+      $('.meng').show();
+      $('.navbox').hide();
+      $('.navclose').show();
+    }
+  })
+  $('.meng').on('click',function(e){
+    $('.meng').hide();
+    $('.navbar').css('right','-100%');
+    $('.navbox').show();
+    $('.navclose').hide();
+  })
+
+  $('.meng').on('touchmove',function(e){
+          e.preventDefault()
+      e.stopPropagation()
+  })
+  $('.navbar').on('touchmove',function(e){
+    e.preventDefault()
+e.stopPropagation()
+})
+})
